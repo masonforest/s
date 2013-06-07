5e21dc6f2b941d94ac9c1c2ed3528e31779487a5
Mason Fischer <mason@thoughtbot.com>
2013-06-07 11:53:34 -0700

    replace

--- s.go
+++ s.go
@@ -1,21 +1,29 @@
 package main
-import "fmt"
 import "regexp"
 import "os"
 import "io/ioutil"
 import "strings"
-
+import "fmt"
 
 func main() {
-  var args = strings.Split(os.Args[1],"/")
-  var replace = args[1]
-  var with = args[2]
+  replace, with, files := parseArgs()
   r, _ := regexp.Compile(replace)
-  var files []os.FileInfo
-  files, _ = ioutil.ReadDir(".")
   for i := range files {
     var content []byte
     content, _ = ioutil.ReadFile(files[i].Name())
-    fmt.Println(r.ReplaceAllString(string(content), with))
+    f, err := os.OpenFile(files[i].Name(), os.O_WRONLY, 0666)
+    println(err)
+    _, err = f.WriteString(r.ReplaceAllString(string(content), with))
+    if err != nil {
+      fmt.Print(err)
+    }
   }
+}
+
+func parseArgs()(string, string, []os.FileInfo){
+  var args = strings.Split(os.Args[1],"/")
+  var replace = args[1]
+  var with = args[2]
+  files, _ := ioutil.ReadDir(".")
+  return replace, with, files
 }
